f4740cc19e1110149c7a84d21a96b7c3f19ca2a3
MikeCaputo <michael.l.caputo@gmail.com>
2024-09-29 09:08:18 -0400

    Small map edits

--- castle-siege/src/helper-methods.js
+++ castle-siege/src/helper-methods.js
@@ -162,7 +162,7 @@ export const generateGamePrologue = async (openai, commanderInfo, selectedMap) =
     }
   });
 
-  const maxWordCount = 500; // And unaltered test was 693, too long. I tried 400 and was nice and snappy! I'll try 500 to see if I get a _little_ more description. But I think this is a good way to control the length.
+  const maxWordCount = 450; // And unaltered test was 693, too long. I tried 400 and was nice and snappy! I'll try 500 to see if I get a _little_ more description. But I think this is a good way to control the length. update: nudging this down to 450.
 
   const aiInstructionPrompt = `A group of MtG commander(s) are cooperatively allied in a great battle against a powerful enemy. A description of the commander(s) is: ${commanderInfo}. Please write a prologue of their great battle against ${selectedMap.name}, which will take place in the places of ${enemyBaseNames}, and will include a mystery or quest which is thematic to the commanders and setting. Please only include a portion of this mystery or quest; it will be concluded at a later time. Please limit the response to no more than ${maxWordCount} words.`;
   console.log(`aiInstructionPrompt is: ${aiInstructionPrompt}`);
