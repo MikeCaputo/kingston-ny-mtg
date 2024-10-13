016203b629048e249cfcbc920d121840f8b96827
MikeCaputo <michael.l.caputo@gmail.com>
2024-10-13 09:18:55 -0400

    Refine grid size and make centering dynamic

--- castle-siege/src/HexGrid.jsx
+++ castle-siege/src/HexGrid.jsx
@@ -5,6 +5,7 @@ import { areArraysEqual } from './helper-methods.js';
 const HexGrid = (props) => {
   const hexSize = 75;
   const [hexArray, setHexArray] = useState([]);
+  const [hexGridWidthInPixels, setHexGridWidthInPixels] = useState(0);
 
   // Map data
   const selectedMap = props.selectedMap;
@@ -22,21 +23,22 @@ const HexGrid = (props) => {
     setHexArray([]);
 
     const hexes = [];
-    // Start with a small number, just to I can do simple dev prototyping.
-    // WIP!! I need to move this to the _map data_. Each map can be different. And then I won't have to do weird matching of which hex gets an enemy base.
     const cols = selectedMap.hexGridWidth;
     const rows = selectedMap.hexGridHeight;
 
+    console.log('map cols; should be width: ', cols)
+    console.log('map rows; should be height: ', rows)
+
     // MC note: ChatGPT started me with this. Then I nudged the numbers around until I got what I wanted.
-    // ugh, some of the row nad col geometry is flipped here. I'll fix that later; not a blocker atm.
-    for (let row = 0; row < rows; row++) {
-      for (let col = 0; col < cols; col++) {
+    // ugh, some of the row nad col geometry is flipped here. I'll fix that later; not a blocker atm. . -- next.
+    for (let col = 0; col < cols; col++) {
+      for (let row = 0; row < rows; row++) {
         const xOffset = (row % 2) * hexSize * 1.55;
         const x = col * hexSize * 3.10 + xOffset;
         const y = row * hexSize * 0.85;
 
         // here....need to see if there is a base here.
-        // might be able to refactor this into a "deep search".
+        // might be able to refactor this into a "deep search". . -- then next, make sure as much of this is moved to the map schema.
         let enemyBaseAtThisHex = null;
         for(let baseIndex = 0; baseIndex < selectedMap.enemyBases.length; baseIndex++){
           const thisEnemyBase = selectedMap.enemyBases[baseIndex];
@@ -64,36 +66,36 @@ const HexGrid = (props) => {
         });
       }
     }
-    // console.log('populate hexArray...? hexes are: ', hexes)
     setHexArray(hexes); // Store the hex grid in state
-  };
 
-  // const handleHexClick = (id) => {
-  //   console.log(`Hex clicked: ${id}`);
-  // };
+    const hexWidthWhenRendered = 237; // Manually calcuated this after rendered in the browser. Used to determine the grid width to allow for css centering.
+    setHexGridWidthInPixels(selectedMap.hexGridWidth *  hexWidthWhenRendered)
+  };
 
   return (
     <div className="hex-grid">
-      {hexArray.map((hex) => (
-        <Hex
-          key={hex.id}
-          id={hex.id}
-          x={hex.x}
-          y={hex.y}
-          col={hex.col}
-          row={hex.row}
-          size={hex.size}
-          enemyBaseAtThisHex={hex.enemyBaseAtThisHex}
-          commandersArray={props.commandersArray}
-          setCommandersArray={props.setCommandersArray}
-          addToGameLog={props.addToGameLog}
-          generateGameSummary={props.generateGameSummary}
-          openai={props.openai}
-          setIsModalOpen={props.setIsModalOpen}
-          populateModal={props.populateModal}
-        />
-      ))}
-      {/* <button onClick={createHexGrid}>Create Hex Grid</button> */}
+      <div className="hex-grid-inner" style={{ width: `${hexGridWidthInPixels}px` }}>
+        {hexArray.map((hex) => (
+          <Hex
+            key={hex.id}
+            id={hex.id}
+            x={hex.x}
+            y={hex.y}
+            col={hex.col}
+            row={hex.row}
+            size={hex.size}
+            enemyBaseAtThisHex={hex.enemyBaseAtThisHex}
+            commandersArray={props.commandersArray}
+            setCommandersArray={props.setCommandersArray}
+            addToGameLog={props.addToGameLog}
+            generateGameSummary={props.generateGameSummary}
+            openai={props.openai}
+            setIsModalOpen={props.setIsModalOpen}
+            populateModal={props.populateModal}
+          />
+        ))}
+        {/* <button onClick={createHexGrid}>Create Hex Grid</button> */}
+      </div>
     </div>
   );
 };
